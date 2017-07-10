e1516fc072fd20fea04fe63aae2048207fbf259e
kentanvictor <2030457926@qq.com>
2017-07-10 11:02:58 +0800

    second commit

--- app/src/main/java/com/example/dell/bmob_demo/MainActivity.java
+++ app/src/main/java/com/example/dell/bmob_demo/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.dell.bmob_demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.dell.bmob_demo.json.Person;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button add;
+    private Button delete;
+    private Button change;
+    private Button search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bmob.initialize(this, "fc49e0d873ae4a3d3abf55f7062ed079");
+        init();
+        add.setOnClickListener(this);
+    }
+    public void init()
+    {
+        add = (Button) findViewById(R.id.add_but);
+        delete = (Button) findViewById(R.id.delete_but1);
+        change = (Button) findViewById(R.id.change_but1);
+        search = (Button) findViewById(R.id.search_but1);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.add_but:
+                Person p2 = new Person();
+                p2.setName("KenTan");
+                p2.setAddress("廣東廣州");
+                p2.save(new SaveListener<String>() {
+                    @Override
+                    public void done(String objectId,BmobException e) {
+                        if(e==null){
+                            Toast.makeText(MainActivity.this, "添加数据成功，返回objectId为："+objectId, Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(MainActivity.this, "创建数据失败：" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                break;
+            case R.id.delete_but1:
+                break;
+            case R.id.change_but1:
+                break;
+            case R.id.search_but1:
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/dell/bmob_demo/SecondActivity.java
@@ -0,0 +1,8 @@
+package com.example.dell.bmob_demo;
+
+/*
+ * Created by KenTan on 2017/7/10.
+ */
+
+public class SecondActivity {
+}
