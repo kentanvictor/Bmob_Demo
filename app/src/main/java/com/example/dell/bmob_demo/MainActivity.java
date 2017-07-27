10b2166cfce075019c17e6a2ee27f9bee6727d7d
kentanvictor <2030457926@qq.com>
2017-07-27 19:21:21 +0800

    更新數組數據(fruitlessly)

--- app/src/main/java/com/example/dell/bmob_demo/MainActivity.java
+++ app/src/main/java/com/example/dell/bmob_demo/MainActivity.java
@@ -216,31 +216,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void done(String objectId, BmobException e) {
                 if (e == null) {
-                    Log.i("bmob", "保存成功");
+                    showToast("bmob 保存成功");
                 } else {
-                    Log.i("bmob", "保存失败：" + e.getMessage());
+                    showToast("bmob 保存失败：" + e.getMessage());
                 }
             }
 
         });
     }
+
     //更新數組數據
     public void updateArray() {
         Person p2 = new Person();
-    //更新String类型数组中的值
+        //更新String类型数组中的值
         p2.setValue("hobbys.0", "爬山"); //将hobbys中第一个位置的爱好（上面添加成功的唱歌）修改为爬山
-    //更新Object类型数组中的某个位置的对象值(0对应集合中第一个元素)
+        //更新Object类型数组中的某个位置的对象值(0对应集合中第一个元素)
         p2.setValue("cards.0", new Person.BankCard("中行", "中行卡号"));    //将cards中第一个位置银行卡修改为指定BankCard对象
-    //更新Object类型数组中指定对象的指定字段的值
-    //p2.setValue("cards.0.bankName", "农行卡");  //将cards中第一个位置的银行卡名称修改为农行卡
-    //p2.setValue("cards.1.cardNumber", "农行卡账号"); //将cards中第二个位置的银行卡账号修改为农行卡账号
+        //更新Object类型数组中指定对象的指定字段的值
+        //p2.setValue("cards.0.bankName", "农行卡");  //将cards中第一个位置的银行卡名称修改为农行卡
+        //p2.setValue("cards.1.cardNumber", "农行卡账号"); //将cards中第二个位置的银行卡账号修改为农行卡账号
         p2.update((String) objectId, new UpdateListener() {
             @Override
             public void done(BmobException e) {
                 if (e == null) {
-                    Log.i("bmob", "成功");
+                    showToast("bmob 保存成功");
                 } else {
-                    Log.i("bmob", "失败：" + e.getMessage());
+                    showToast("bmob 保存失败：" + e.getMessage());
                 }
             }
         });
