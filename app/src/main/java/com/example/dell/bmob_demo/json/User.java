f466c957456b1a385bdc0cd1223c8d6349f9528d
kentanvictor <2030457926@qq.com>
2017-07-30 16:45:48 +0800

    更新數組數據(helpless)

---
+++ app/src/main/java/com/example/dell/bmob_demo/json/User.java
@@ -0,0 +1,62 @@
+package com.example.dell.bmob_demo.json;
+
+/*
+ * Created by KenTan on 2017/7/30.
+ */
+
+import cn.bmob.v3.BmobUser;
+
+public class User extends BmobUser {
+    private String userName;
+    private int password;
+    private String userEmail;
+    private boolean emailVerified;
+    private String mobilePhoneNumber;
+    private boolean mobilePhoneNumberVerified;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+    public boolean isEmailVerified() {
+        return emailVerified;
+    }
+
+    public void setEmailVerified(boolean emailVerified) {
+        this.emailVerified = emailVerified;
+    }
+    @Override
+    public String getMobilePhoneNumber() {
+        return mobilePhoneNumber;
+    }
+
+    public void setMobilePhoneNumber(String mobilePhoneNumber) {
+        this.mobilePhoneNumber = mobilePhoneNumber;
+    }
+    public boolean isMobilePhoneNumberVerified() {
+        return mobilePhoneNumberVerified;
+    }
+
+    public void setMobilePhoneNumberVerified(boolean mobilePhoneNumberVerified) {
+        this.mobilePhoneNumberVerified = mobilePhoneNumberVerified;
+    }
+}
