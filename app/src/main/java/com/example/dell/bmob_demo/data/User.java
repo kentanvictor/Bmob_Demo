3ad0f2cd37c0668bde441396e16257e1eb2c0203
kentanvictor <2030457926@qq.com>
2017-08-07 10:30:23 +0800

    註冊用戶(helpless)

--- app/src/main/java/com/example/dell/bmob_demo/data/User.java
+++ app/src/main/java/com/example/dell/bmob_demo/data/User.java
@@ -1,19 +1,20 @@
 package com.example.dell.bmob_demo.data;
+
 import cn.bmob.v3.BmobUser;
 import cn.bmob.v3.datatype.BmobFile;
 
-/**
- * Created by DELL on 2017/8/6.
+/*
+ * Created by KenTan on 2017/8/6.
  */
 
-public class User extends BmobUser{
+public class User extends BmobUser {
     private BmobFile IDCard_img;
-    public BmobFile getIDCard_img()
-    {
+
+    public BmobFile getIDCard_img() {
         return IDCard_img;
     }
-    public void setIDCard_img(BmobFile IDCard_img)
-    {
+
+    public void setIDCard_img(BmobFile IDCard_img) {
         this.IDCard_img = IDCard_img;
     }
 }
